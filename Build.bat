1a03b0da49ef909325a278de2abfd0f43aa2f523
Baoquan Zuo <baoquan.zuo@gmail.com>
2010-01-31 06:24:12 +0000

    Fixed a bug of Build.bat

--- Build.bat
+++ Build.bat
@@ -4,15 +4,15 @@ if "%1" == "" goto :Usage
 
 ECHO Clean...
 REM msbuild.exe /nologo /t:Clean /p:Config=%1 "Delphi Spring Framework.groupproj"
-msbuild.exe /nologo /t:Clean /p:Config=%1 "Spring.Base.dproj"
-msbuild.exe /nologo /t:Clean /p:Config=%1 "Spring.Core.dproj"
-msbuild.exe /nologo /t:Clean /p:Config=%1 "Spring.Extensions.dproj"
+msbuild.exe /nologo /t:Clean /p:Config=%1 "Packages\Spring.Base.dproj"
+msbuild.exe /nologo /t:Clean /p:Config=%1 "Packages\Spring.Core.dproj"
+msbuild.exe /nologo /t:Clean /p:Config=%1 "Packages\Spring.Extensions.dproj"
 
 ECHO Building...
 REM msbuild.exe /nologo /t:Build /p:Config=%1 "Delphi Spring Framework.groupproj"
-msbuild.exe /nologo /t:Build /p:Config=%1 "Spring.Base.dproj"
-msbuild.exe /nologo /t:Build /p:Config=%1 "Spring.Core.dproj"
-msbuild.exe /nologo /t:Build /p:Config=%1 "Spring.Extensions.dproj"
+msbuild.exe /nologo /t:Build /p:Config=%1 "Packages\Spring.Base.dproj"
+msbuild.exe /nologo /t:Build /p:Config=%1 "Packages\Spring.Core.dproj"
+msbuild.exe /nologo /t:Build /p:Config=%1 "Packages\Spring.Extensions.dproj"
 
 GOTO :End
 
