a56a08466b57d20c869bcfa807d50b8196dd2aab
Leo Raster <leo.raster@gmail.com>
2010-04-22 17:39:06 +0000

    - some of ValueConverters implementation

--- Source/Base/Spring.ValueConverters.pas
+++ Source/Base/Spring.ValueConverters.pas
@@ -35,10 +35,10 @@ type
   IValueConverter = interface
   ['{4FCF1210-7B19-4B67-9B71-DE715D80D5EE}']
     function ConvertTo(const value: TValue;
-      const sourceTypeInfo: PTypeInfo;
+      const targetTypeInfo: PTypeInfo;
       const parameter: TValue): TValue;
     function ConvertFrom(const value: TValue;
-      const targetTypeInfo: PTypeInfo;
+      const sourceTypeInfo: PTypeInfo;
       const parameter: TValue): TValue;
   end;
 
@@ -72,13 +72,19 @@ type
   TValueConverterClass = class of TValueConverter;
 
   /// <summary>
-  /// Default value Converter makes the exact conversion
+  ///  Default value Converter makes the exact conversion
   /// </summary>
   /// <remarks>
   ///  There is three steps of doing so
   ///  1. find/lock "global" registry
   ///  2. use TValue.TryCast
-  ///  3. use RTTI exploring
+  ///  3. use RTTI exploring and select apropriate converter
+  ///  There are four different internall converter types
+  ///  that can be selected to convert
+  ///   * TNullable<T> and T
+  ///   * Enumeration and Integer/string
+  ///   * TColor and Integer/string
+  ///   * Integer and string
   /// </remarks>
   TDefaultValueConverter = class(TValueConverter)
   protected
@@ -91,16 +97,9 @@ type
   end;
 
   /// <summary>
-  /// Provides a way to select appropriate Converter by using Rtti
+  ///  Simply provides an conversion routine beetwen Integer and string
   /// </summary>
-  /// <remarks>
-  ///  There is three different internall converter types
-  ///  that can be selected to convert
-  ///  * TNullable<T> and T
-  ///  * Enumeration and Integer/string
-  ///  * TColor and Integer/string
-  /// </remarks>
-  TRttiValueConverter = class(TValueConverter)
+  TIntegerToStringConverter = class(TValueConverter)
   protected
     function SourceToTarget(const value: TValue;
       const targetTypeInfo: PTypeInfo;
@@ -111,11 +110,54 @@ type
   end;
 
   /// <summary>
-  /// Provides a registry to store information about available converters that can be use
-  /// to make a conversion beetwen each type
+  ///  Simply provides an conversion routine beetwen Boolean and string
+  /// </summary>
+  TBooleanToStringConverter = class(TValueConverter)
+  protected
+    function SourceToTarget(const value: TValue;
+      const targetTypeInfo: PTypeInfo;
+      const parameter: TValue): TValue; override;
+    function TargetToSource(const value: TValue;
+      const sourceTypeInfo: PTypeInfo;
+      const parameter: TValue): TValue; override;
+  end;
+
+  /// <summary>
+  ///  Provides an conversion routine beetwen TNullable<T> and T
+  /// </summary>
+  /// <remarks>
+  ///  Internally it may use another Converter delegating
+  ///  conversion routine
+  /// </remarks>
+  TNullableValueConverter = class(TValueConverter)
+  protected
+    function SourceToTarget(const value: TValue;
+      const targetTypeInfo: PTypeInfo;
+      const parameter: TValue): TValue; override;
+    function TargetToSource(const value: TValue;
+      const sourceTypeInfo: PTypeInfo;
+      const parameter: TValue): TValue; override;
+  end;
+
+  /// <summary>
+  ///  Provides an conversion routine beetwen enum and Integer/string
+  /// </summary>
+  TEnumValueConverter = class(TValueConverter)
+  protected
+    function SourceToTarget(const value: TValue;
+      const targetTypeInfo: PTypeInfo;
+      const parameter: TValue): TValue; override;
+    function TargetToSource(const value: TValue;
+      const sourceTypeInfo: PTypeInfo;
+      const parameter: TValue): TValue; override;
+  end;
+
+  /// <summary>
+  ///  Provides a registry to store information about available converters that can be use
+  ///  to make a conversion beetwen each type
   /// </summary>
   TValueConverterFactory = class
-  private
+  strict private
     type
       TConvertedTypeInfo = record
         SourceTypeInfo: PTypeInfo;
@@ -131,11 +173,16 @@ type
     class constructor Create;
     class destructor Destroy;
     class procedure RegisterConverter(const sourceTypeInfo, targetTypeInfo: PTypeInfo;
-      converterClass: TClass);
-    class function CreateConverter(const sourceTypeInfo, targetTypeInfo: PTypeInfo): IValueConverter;
+      converterClass: TValueConverterClass);
+    class function CreateConverter(const sourceTypeInfo,
+      targetTypeInfo: PTypeInfo): IValueConverter;
   end;
 
 implementation
+
+uses
+  StrUtils,
+  SysUtils;
 
 {$REGION 'TValueConverter'}
 
@@ -157,13 +204,13 @@ end;
 function TValueConverter.ConvertTo(const value: TValue;
   const targetTypeInfo: PTypeInfo; const parameter: TValue): TValue;
 begin
-  SourceToTarget(value, targetTypeInfo, parameter);
+  Exit(SourceToTarget(value, targetTypeInfo, parameter));
 end;
 
 function TValueConverter.ConvertFrom(const value: TValue;
   const sourceTypeInfo: PTypeInfo; const parameter: TValue): TValue;
 begin
-  TargetToSource(value, sourceTypeInfo, parameter);
+  Exit(TargetToSource(value, sourceTypeInfo, parameter));
 end;
 
 {$ENDREGION}
@@ -173,8 +220,40 @@ end;
 
 function TDefaultValueConverter.SourceToTarget(const value: TValue;
   const targetTypeInfo: PTypeInfo; const parameter: TValue): TValue;
+var
+  converter: IValueConverter;
+  converterClass: TValueConverterClass;
 begin
-
+  ///  1. find/lock "global" registry
+  converter := TValueConverterFactory.CreateConverter(value.TypeInfo, targetTypeInfo);
+  if converter <> nil then
+    Exit(converter.ConvertTo(value, targetTypeInfo, parameter))
+  else ///  2. use TValue.TryCast
+  if not value.TryCast(targetTypeInfo, Result) then
+  begin
+    ///  3. use RTTI exploring and select apropriate converter then register it
+    ///   * TNullable<T> and T
+    ///   * Enumeration and Integer/string
+    ///   * TColor and Integer/string
+    ///   * Integer and string
+    if StrUtils.ContainsStr(string(value.TypeInfo.Name), 'TNullable') and
+      (targetTypeInfo.Kind in [tkString, tkInteger, tkFloat]) then
+    begin
+      converterClass := TNullableValueConverter;
+    end
+    else
+    if (value.TypeInfo.Kind = tkEnumeration) and
+      (targetTypeInfo.Kind in [tkString, tkInteger, tkFloat]) then
+    begin
+      converterClass := TEnumValueConverter;
+    end;
+    if converterClass <> nil then
+    begin
+      TValueConverterFactory.RegisterConverter(value.TypeInfo,
+        targetTypeInfo, converterClass);
+      Self.SourceToTarget(value, targetTypeInfo, parameter);
+    end;
+  end;
 end;
 
 function TDefaultValueConverter.TargetToSource(const value: TValue;
@@ -186,15 +265,97 @@ end;
 {$ENDREGION}
 
 
-{$REGION 'TRttiValueConverter'}
+{$REGION 'TIntegerToStringConverter'}
 
-function TRttiValueConverter.SourceToTarget(const value: TValue;
+function TIntegerToStringConverter.SourceToTarget(const value: TValue;
+  const targetTypeInfo: PTypeInfo; const parameter: TValue): TValue;
+begin
+  try
+    Result.From<string>(IntToStr(Value.AsInteger));
+  except
+  end;
+end;
+
+function TIntegerToStringConverter.TargetToSource(const value: TValue;
+  const sourceTypeInfo: PTypeInfo; const parameter: TValue): TValue;
+begin
+  try
+    Result.From<Integer>(StrToInt(Value.AsString));
+  except
+  end;
+end;
+
+{$ENDREGION}
+
+
+{$REGION 'TBooleanToStringConverter'}
+
+function TBooleanToStringConverter.SourceToTarget(const value: TValue;
+  const targetTypeInfo: PTypeInfo; const parameter: TValue): TValue;
+var
+  token: string;
+begin
+  if not parameter.IsEmpty then
+    token := parameter.AsString
+  else
+    token := 'False;True';
+  if not Value.AsBoolean then
+    token := Copy(token, 1, Pos(';', token) - 1)
+  else
+    token := Copy(token, Pos(';', token) + 1, Length(token));
+  try
+    Result := TValue.From<string>(token);
+  except
+  end;
+end;
+
+function TBooleanToStringConverter.TargetToSource(const value: TValue;
+  const sourceTypeInfo: PTypeInfo; const parameter: TValue): TValue;
+var
+  token: string;
+begin
+  if not parameter.IsEmpty then
+  begin
+    token := Parameter.AsString;
+    token := Copy(token, 1, Pos(';', token) - 1);
+  end
+  else
+    token := 'False';
+  try
+    TValue.From<Boolean>(AnsiSameText(Value.AsString, token));
+  except
+  end;
+end;
+
+{$ENDREGION}
+
+
+{$REGION 'TNullableValueConverter'}
+
+function TNullableValueConverter.SourceToTarget(const value: TValue;
   const targetTypeInfo: PTypeInfo; const parameter: TValue): TValue;
 begin
 
 end;
 
-function TRttiValueConverter.TargetToSource(const value: TValue;
+function TNullableValueConverter.TargetToSource(const value: TValue;
+  const sourceTypeInfo: PTypeInfo; const parameter: TValue): TValue;
+begin
+
+end;
+
+{$ENDREGION}
+
+
+{$REGION 'TEnumValueConverter'}
+
+function TEnumValueConverter.SourceToTarget(const value: TValue;
+  const targetTypeInfo: PTypeInfo; const parameter: TValue): TValue;
+begin
+
+end;
+
+function TEnumValueConverter.TargetToSource(const value: TValue;
   const sourceTypeInfo: PTypeInfo; const parameter: TValue): TValue;
 begin
 
@@ -221,8 +382,8 @@ var
   pair: TPair<TConvertedTypeInfo, TConverterPackage>;
   value: TConverterPackage;
 begin
+  System.MonitorEnter(fRegistry);
   try
-    System.MonitorEnter(fRegistry);
     for pair in fRegistry do
     begin
       if (pair.Key.SourceTypeInfo = sourceTypeInfo) and
@@ -243,15 +404,26 @@ begin
 end;
 
 class procedure TValueConverterFactory.RegisterConverter(const sourceTypeInfo,
-  targetTypeInfo: PTypeInfo; converterClass: TClass);
+  targetTypeInfo: PTypeInfo; converterClass: TValueConverterClass);
+var
+  value: TConverterPackage;
+  key: TConvertedTypeInfo;
 begin
   System.MonitorEnter(fRegistry);
-  //
-  // register the converter here
-  //
-  System.MonitorExit(fRegistry);
+  try
+    value.ConverterClass := converterClass;
+    with key do
+    begin
+      SourceTypeInfo := sourceTypeInfo;
+      TargetTypeInfo := targetTypeInfo;
+    end;
+    fRegistry.AddOrSetValue(key, value);
+  finally
+    System.MonitorExit(fRegistry);
+  end;
 end;
 
 {$ENDREGION}
+
 
 end.
