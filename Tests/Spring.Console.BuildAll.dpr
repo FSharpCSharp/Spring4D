7bb002c42a78703b467170d19a7935111ef6eae6
Jeroen Wiert Pluimers <jeroen.bitbucket.com@pluimers.com>
2013-11-19 21:10:24 +0100

    Build project for checking which other platforms Spring4D can be compiled on. First try: Win64 and Mac OS X.

---
+++ Tests/Spring.Console.BuildAll.dpr
@@ -0,0 +1,40 @@
+program Spring.Console.BuildAll;
+
+{$APPTYPE CONSOLE}
+
+uses
+  System.SysUtils,
+  Spring in '..\Source\Base\Spring.pas',
+  Spring.Collections in '..\Source\Base\Collections\Spring.Collections.pas',
+  Spring.Collections.Base in '..\Source\Base\Collections\Spring.Collections.Base.pas',
+  Spring.Collections.Dictionaries in '..\Source\Base\Collections\Spring.Collections.Dictionaries.pas',
+  Spring.Collections.Events in '..\Source\Base\Collections\Spring.Collections.Events.pas',
+  Spring.Collections.Extensions in '..\Source\Base\Collections\Spring.Collections.Extensions.pas',
+  Spring.Collections.Lists in '..\Source\Base\Collections\Spring.Collections.Lists.pas',
+  Spring.Collections.Queues in '..\Source\Base\Collections\Spring.Collections.Queues.pas',
+  Spring.Collections.Sets in '..\Source\Base\Collections\Spring.Collections.Sets.pas',
+  Spring.Collections.Stacks in '..\Source\Base\Collections\Spring.Collections.Stacks.pas',
+  Spring.DesignPatterns in '..\Source\Base\Spring.DesignPatterns.pas',
+  Spring.Events in '..\Source\Base\Spring.Events.pas',
+  Spring.Events.Base in '..\Source\Base\Spring.Events.Base.pas',
+  Spring.Helpers in '..\Source\Base\Spring.Helpers.pas',
+  Spring.Reflection in '..\Source\Base\Reflection\Spring.Reflection.pas',
+  Spring.Reflection.ValueConverters in '..\Source\Base\Reflection\Spring.Reflection.ValueConverters.pas',
+  Spring.ResourceStrings in '..\Source\Base\Spring.ResourceStrings.pas',
+  Spring.SystemUtils in '..\Source\Base\Spring.SystemUtils.pas',
+  Spring.Container in '..\Source\Core\Container\Spring.Container.pas',
+  Spring.Container.Builder in '..\Source\Core\Container\Spring.Container.Builder.pas',
+  Spring.Container.ComponentActivator in '..\Source\Core\Container\Spring.Container.ComponentActivator.pas',
+  Spring.Container.Core in '..\Source\Core\Container\Spring.Container.Core.pas',
+  Spring.Container.Extensions in '..\Source\Core\Container\Spring.Container.Extensions.pas',
+  Spring.Container.Injection in '..\Source\Core\Container\Spring.Container.Injection.pas',
+  Spring.Container.LifetimeManager in '..\Source\Core\Container\Spring.Container.LifetimeManager.pas',
+  Spring.Container.Pool in '..\Source\Core\Container\Spring.Container.Pool.pas',
+  Spring.Container.Registration in '..\Source\Core\Container\Spring.Container.Registration.pas',
+  Spring.Container.Resolvers in '..\Source\Core\Container\Spring.Container.Resolvers.pas',
+  Spring.Container.ResourceStrings in '..\Source\Core\Container\Spring.Container.ResourceStrings.pas',
+  Spring.Services in '..\Source\Core\Services\Spring.Services.pas',
+  Spring.Services.Logging in '..\Source\Core\Services\Spring.Services.Logging.pas';
+
+begin
+end.
