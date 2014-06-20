b9bdcb4d57967fbea4b530938c69a2aa3938d975
Linas Naginionis <lnaginionis@gmail.com>
2014-06-21 02:07:06 +0300

    Proxy repository test for SQL databases

--- Test/TestSimpleRepository.pas
+++ Test/TestSimpleRepository.pas
@@ -7,20 +7,43 @@ interface
 uses
   TestFramework, Windows, Forms, Dialogs, Controls, Classes, SysUtils,
   Variants, Graphics, Messages, StdCtrls, Core.Session, Core.Interfaces
-  ,uModels, Rtti, SQLiteTable3;
+  ,uModels, Rtti, SQLiteTable3, Mapping.Attributes, Core.Repository.Proxy;
 
 type
+  ICustomerRepository = interface(IPagedRepository<TCustomer, Integer>)
+    ['{955BF130-3E2F-45E2-A9E9-79647CA3F33B}']
+
+    [Query('SELECT * FROM CUSTOMERS WHERE CUSTNAME = :0')]
+    function FindByName(const AName: string): TCustomer;
+  end;
+
   TSimpleRepositoryTests = class(TTestCase)
   private
     FConnection: IDBConnection;
     FSession: TSession;
     FRepository: IPagedRepository<TCustomer,Integer>;
+  protected
+    function CreateRepository(): IInterface; virtual;
+  public
+    procedure SetUp; override;
+    procedure TearDown; override;
+
+    property Session: TSession read FSession write FSession;
+    property Connection: IDBConnection read FConnection write FConnection;
+    property Repository: IPagedRepository<TCustomer, Integer> read FRepository write FRepository;
+  published
+    procedure FindOne();
+    procedure Count();
+  end;
+
+  TCustomRepositoryTests = class(TSimpleRepositoryTests)
+  private
+    FCustomerRepository: ICustomerRepository;
   public
     procedure SetUp; override;
     procedure TearDown; override;
   published
-    procedure FindOne();
-    procedure Count();
+    procedure FindByName();
   end;
 
 
@@ -53,6 +76,11 @@ begin
   CheckEquals(2, FRepository.Count);
 end;
 
+function TSimpleRepositoryTests.CreateRepository: IInterface;
+begin
+  Result := TSimpleRepository<TCustomer, Integer>.Create(FSession);
+end;
+
 procedure TSimpleRepositoryTests.FindOne;
 var
   LCustomer: TCustomer;
@@ -70,7 +98,7 @@ begin
   inherited;
   FConnection := TConnectionFactory.GetInstance(dtSQLite, TestDB);
   FSession := TSession.Create(FConnection);
-  FRepository := TSimpleRepository<TCustomer, Integer>.Create(FSession);
+  FRepository := CreateRepository as IPagedRepository<TCustomer,Integer>;
 
   FConnection.AddExecutionListener(
     procedure(const ACommand: string; const AParams: IList<TDBParam>)
@@ -95,7 +123,32 @@ begin
   FSession.Free;
 end;
 
+{ TCustomRepositoryTests }
+
+procedure TCustomRepositoryTests.FindByName;
+var
+  LCustomer: TCustomer;
+begin
+  InsertCustomer(10, 'Foo');
+  LCustomer := FCustomerRepository.FindByName('Foo');
+  CheckEquals('Foo', LCustomer.Name);
+  LCustomer.Free;
+end;
+
+procedure TCustomRepositoryTests.SetUp;
+begin
+  inherited;
+  FCustomerRepository := TProxyRepository<TCustomer, Integer>.Create(FSession, TypeInfo(ICustomerRepository)) as ICustomerRepository;
+end;
+
+procedure TCustomRepositoryTests.TearDown;
+begin
+  inherited;
+
+end;
+
 initialization
   RegisterTest(TSimpleRepositoryTests.Suite);
+  RegisterTest(TCustomRepositoryTests.Suite);
 
 end.
