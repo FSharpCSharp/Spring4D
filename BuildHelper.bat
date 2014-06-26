f60314fb9d91780ea24769c36b0748d4a2f88ca0
Jeroen Wiert Pluimers <jeroenp@bitbucket.org>
2014-06-26 21:23:32 +0200

    fix `BuildHelper.bat` to cope with `MSBuildLog.txt` files when the root of the `Spring4D` has a space or other character in it that requires quoting.

--- BuildHelper.bat
+++ BuildHelper.bat
@@ -8,7 +8,7 @@ echo %6
 echo "%7"
 CALL %1
 setlocal
-for /f "tokens=2,4" %%c in ('echo %~4 %~3') do set buildlog=%~dp2%%c.%%d.MSBuildLog.txt
+for /f "tokens=2,4" %%c in ('echo %~4 %~3') do set buildlog="%~dp2%%c.%%d.MSBuildLog.txt"
 echo %buildlog%
 %FrameworkDir%\msbuild.exe /nologo %2 /target:build /p:DCC_BuildAllUnits=true /p:%3 /p:%4 /p:%5 /p:%6 /l:FileLogger,Microsoft.Build.Engine;logfile=%buildlog%
 endlocal
