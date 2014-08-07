3d8615fc08ea10833fa9ee036801ad70885dc467
Cesar Romero <cesarliws@gmail.com>
2014-08-07 16:27:43 -0300

    !  Fixed TUIBStatementAdapter.SetParams - Uncomment code from previous build

--- Marshmallow/SQL.Generators.Register.pas
+++ Marshmallow/SQL.Generators.Register.pas
@@ -2,18 +2,15 @@ unit SQL.Generators.Register;
 
 interface
 
-  //register all implemented generators
-
+// register all implemented generators
 uses
-// TODO -oCesar : Check how to register in the package Spring.Persitence.Adapter.SQLite
-//  SQL.Generator.SQLite3,
-  SQL.Generator.PostgreSQL
-  ,SQL.Generator.Oracle
-  ,SQL.Generator.MSSQL
-  ,SQL.Generator.MySQL
-  ,SQL.Generator.Firebird
-  ,SQL.Generator.ASA
-  ;
+  SQL.Generator.SQLite3,
+  SQL.Generator.PostgreSQL,
+  SQL.Generator.Oracle,
+  SQL.Generator.MSSQL,
+  SQL.Generator.MySQL,
+  SQL.Generator.Firebird,
+  SQL.Generator.ASA;
 
 implementation
 
