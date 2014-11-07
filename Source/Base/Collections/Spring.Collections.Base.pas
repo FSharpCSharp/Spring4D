FSharpCSharp/Spring4D

commit d5370631ef6ae1477ff070eac6b6a16b0ec82567

optimized AddRange and InsertRange for TList<T> (see issue #55)

Source/Base/Collections/Spring.Collections.Base.pas
@@ -346,7 +346,11 @@ type
     destructor Destroy; override;
 
     function Add(const item: T): Integer; reintroduce; virtual;
+    procedure AddRange(const values: array of T); override;
+    procedure AddRange(const collection: IEnumerable<T>); override;
+
     function Remove(const item: T): Boolean; override;
+
     procedure Clear; override;
 
     function First: T; overload; override;
@@ -1471,6 +1475,16 @@ end;
 procedure TListBase<T>.AddInternal(const item: T);
 begin
   Add(item);
+end;
+
+procedure TListBase<T>.AddRange(const values: array of T);
+begin
+  InsertRange(Count, values);
+end;
+
+procedure TListBase<T>.AddRange(const collection: IEnumerable<T>);
+begin
+  InsertRange(Count, collection);
 end;
 
 function TListBase<T>.AsList: IList;

Source/Base/Collections/Spring.Collections.Lists.pas
@@ -110,6 +110,8 @@ type
     function IndexOf(const item: T; index, count: Integer): Integer; override;
 
     procedure Insert(index: Integer; const item: T); override;
+    procedure InsertRange(index: Integer; const values: array of T); override;
+    procedure InsertRange(index: Integer; const collection: IEnumerable<T>); override;
 
     procedure Delete(index: Integer); override;
     procedure DeleteRange(index, count: Integer); override;
@@ -389,6 +391,66 @@ begin
   IncreaseVersion;
 
   Changed(item, caAdded);
+end;
+
+procedure TList<T>.InsertRange(index: Integer; const values: array of T);
+var
+  i: Integer;
+begin
+{$IFDEF SPRING_ENABLE_GUARD}
+  Guard.CheckRange((index >= 0) and (index <= fCount), 'index');
+{$ENDIF}
+
+  EnsureCapacity(fCount + Length(values));
+  if index <> fCount then
+  begin
+    fArrayManager.Move(fItems, index, index + Length(values), fCount - index);
+    fArrayManager.Finalize(fItems, index, Length(values));
+  end;
+{$IFDEF WEAKREF}
+  if HasWeakRef then
+    for i := Low(values) to High(values) do
+      fItems[index + i] := values[i]
+  else
+{$ENDIF}
+    System.Move(values[0], fItems[index], Length(values) * SizeOf(T));
+//    fArrayManager.Move(values, fItems, 0, index, Length(values));
+  Inc(fCount, Length(values));
+  IncreaseVersion;
+
+  for i := Low(values) to High(values) do
+    Changed(values[i], caAdded);
+end;
+
+procedure TList<T>.InsertRange(index: Integer;
+  const collection: IEnumerable<T>);
+var
+  list: TList<T>;
+  i: Integer;
+begin
+  if collection.AsObject is TList<T> then
+  begin
+    list := TList<T>(collection.AsObject);
+
+{$IFDEF SPRING_ENABLE_GUARD}
+    Guard.CheckRange((index >= 0) and (index <= fCount), 'index');
+{$ENDIF}
+
+    EnsureCapacity(fCount + Length(list.fItems));
+    if index <> fCount then
+    begin
+      fArrayManager.Move(fItems, index, index + Length(list.fItems), fCount - index);
+      fArrayManager.Finalize(fItems, index, Length(list.fItems));
+    end;
+    fArrayManager.Move(list.fItems, fItems, 0, index, list.fCount);
+    Inc(fCount, list.fCount);
+    IncreaseVersion;
+
+    for i := 0 to list.fCount - 1 do
+      Changed(list.fItems[i], caAdded);
+  end
+  else
+    inherited InsertRange(index, collection);
 end;
 
 procedure TList<T>.DeleteInternal(index: Integer;

commit d53917523c5558edcbb0c6d2959835db82c0a028

fixed bug with wrong handling of managed types introduced in d537063

Source/Base/Collections/Spring.Collections.Base.pas
@@ -122,8 +122,9 @@ type
     class function GetEqualityComparer: IEqualityComparer<T>; static;
   {$ENDREGION}
 {$IFDEF WEAKREF}
-    function HasWeakRef: Boolean;
+    function HasWeakRef: Boolean; inline;
 {$ENDIF}
+    function IsManaged: Boolean; inline;
     function TryGetElementAt(out value: T; index: Integer): Boolean; virtual;
     function TryGetFirst(out value: T): Boolean; overload; virtual;
     function TryGetFirst(out value: T; const predicate: TPredicate<T>): Boolean; overload;
@@ -400,6 +401,7 @@ type
 implementation
 
 uses
+  Rtti,
   TypInfo,
   Spring.Collections.Adapters,
   Spring.Collections.Events,
@@ -760,10 +762,23 @@ begin
   Result := fEqualityComparer;
 end;
 
+function TEnumerableBase<T>.IsManaged: Boolean;
+begin
+{$IFDEF DELPHIXE7_UP}
+  Result := IsManagedType(T);
+{$ELSE}
+  Result := Rtti.IsManaged(TypeInfo(T));
+{$ENDIF}
+end;
+
 {$IFDEF WEAKREF}
 function TEnumerableBase<T>.HasWeakRef: Boolean;
 begin
+{$IFDEF DELPHIXE7_UP}
+  Result := System.HasWeakRef(T);
+{$ELSE}
   Result := System.TypInfo.HasWeakRef(TypeInfo(T));
+{$ENDIF}
 end;
 {$ENDIF}
 

Source/Base/Collections/Spring.Collections.Lists.pas
@@ -441,14 +441,13 @@ begin
     fArrayManager.Move(fItems, index, index + Length(values), fCount - index);
     fArrayManager.Finalize(fItems, index, Length(values));
   end;
-{$IFDEF WEAKREF}
-  if HasWeakRef then
-    for i := Low(values) to High(values) do
-      fItems[index + i] := values[i]
+
+  if not IsManaged{$IFDEF WEAKREF} and not HasWeakRef{$ENDIF} then
+    System.Move(values[0], fItems[index], Length(values) * SizeOf(T))
   else
-{$ENDIF}
-    System.Move(values[0], fItems[index], Length(values) * SizeOf(T));
-//    fArrayManager.Move(values, fItems, 0, index, Length(values));
+    for i := Low(values) to High(values) do
+      fItems[index + i] := values[i];
+
   Inc(fCount, Length(values));
   IncreaseVersion;
 
@@ -464,11 +463,11 @@ var
 begin
   if collection.AsObject is TList<T> then
   begin
-    list := TList<T>(collection.AsObject);
-
 {$IFDEF SPRING_ENABLE_GUARD}
     Guard.CheckRange((index >= 0) and (index <= fCount), 'index');
 {$ENDIF}
+
+    list := TList<T>(collection.AsObject);
 
     EnsureCapacity(fCount + Length(list.fItems));
     if index <> fCount then
@@ -476,7 +475,13 @@ begin
       fArrayManager.Move(fItems, index, index + Length(list.fItems), fCount - index);
       fArrayManager.Finalize(fItems, index, Length(list.fItems));
     end;
-    fArrayManager.Move(list.fItems, fItems, 0, index, list.fCount);
+
+    if not IsManaged{$IFDEF WEAKREF} and not HasWeakRef{$ENDIF} then
+      System.Move(list.fItems[0], fItems[index], list.fCount * SizeOf(T))
+    else
+      for i := Low(list.fItems) to list.fCount - 1 do
+        fItems[index + i] := list.fItems[i];
+
     Inc(fCount, list.fCount);
     IncreaseVersion;
 