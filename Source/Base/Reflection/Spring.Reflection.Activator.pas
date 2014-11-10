64eb31bc6b66b489a877631825407d9b14b73520
Stefan Glienke <sglienke@dsharp.org>
2014-11-10 09:59:54 +0100

    added CreateInstance overloads

--- Source/Base/Reflection/Spring.Reflection.Activator.pas
+++ Source/Base/Reflection/Spring.Reflection.Activator.pas
@@ -45,11 +45,20 @@ type
   public
     class function CreateInstance(const classType: TRttiInstanceType): TValue; overload; static;
     class function CreateInstance(const classType: TRttiInstanceType;
-      const constructorMethod: TRttiMethod; const arguments: array of TValue): TValue; overload; static;
-    class function CreateInstance(const classType: TRttiInstanceType;
       const arguments: array of TValue): TValue; overload; static;
+    class function CreateInstance(const classType: TRttiInstanceType;
+      const constructorMethod: TRttiMethod; const arguments: array of TValue): TValue; overload; static;
+
     class function CreateInstance(const typeInfo: PTypeInfo): TValue; overload; static;
     class function CreateInstance(const typeName: string): TValue; overload; static;
+
+    class function CreateInstance(classType: TClass): TObject; overload; static;
+    class function CreateInstance(classType: TClass;
+      const arguments: array of TValue): TObject; overload; static;
+
+    class function CreateInstance<T: class>: T; overload; static;
+    class function CreateInstance<T: class>(
+      const arguments: array of TValue): T; overload; static;
   end;
 
   {$ENDREGION}
@@ -65,17 +74,12 @@ uses
 
 class function TActivator.CreateInstance(
   const classType: TRttiInstanceType): TValue;
-var
-  method: TRttiMethod;
 begin
 {$IFDEF SPRING_ENABLE_GUARD}
   Guard.CheckNotNull(classType, 'classType');
 {$ENDIF}
 
-  for method in classType.GetMethods do
-    if method.IsConstructor and (Length(method.GetParameters) = 0) then
-      Exit(method.Invoke(classType.MetaclassType, []));
-  Result := nil;
+  Result := CreateInstance(classType, []);
 end;
 
 class function TActivator.CreateInstance(const classType: TRttiInstanceType;
@@ -92,7 +96,7 @@ begin
   SetLength(parameterTypes, Length(arguments));
   for i := Low(arguments) to High(arguments) do
     parameterTypes[i] := arguments[i].TypeInfo;
-  constructorMethod := classType.Methods.Single(TMethodFilters.IsConstructor
+  constructorMethod := classType.Methods.First(TMethodFilters.IsConstructor
     and TMethodFilters.HasParameterTypes(parameterTypes));
   Result := CreateInstance(classType, constructorMethod, arguments);
 end;
@@ -117,10 +121,7 @@ begin
 {$ENDIF}
 
   rttiType := TType.GetType(typeInfo);
-  if rttiType is TRttiInstanceType then
-    Result := TActivator.CreateInstance(TRttiInstanceType(rttiType))
-  else
-    Result := nil;
+  Result := TActivator.CreateInstance(rttiType as TRttiInstanceType)
 end;
 
 class function TActivator.CreateInstance(const typeName: string): TValue;
@@ -128,10 +129,43 @@ var
   rttiType: TRttiType;
 begin
   rttiType := TType.FindType(typeName);
-  if rttiType is TRttiInstanceType then
-    Result := TActivator.CreateInstance(TRttiInstanceType(rttiType))
-  else
-    Result := nil;
+  Result := TActivator.CreateInstance(rttiType as TRttiInstanceType)
+end;
+
+class function TActivator.CreateInstance(classType: TClass): TObject;
+begin
+{$IFDEF SPRING_ENABLE_GUARD}
+  Guard.CheckNotNull(classType, 'typeInfo');
+{$ENDIF}
+
+  Result := CreateInstance(classType.ClassInfo).AsObject;
+end;
+
+class function TActivator.CreateInstance(classType: TClass;
+  const arguments: array of TValue): TObject;
+var
+  instanceType: TRttiInstanceType;
+begin
+{$IFDEF SPRING_ENABLE_GUARD}
+  Guard.CheckNotNull(classType, 'typeInfo');
+{$ENDIF}
+
+  instanceType := TType.GetType(classType.ClassInfo) as TRttiInstanceType;
+  Result := CreateInstance(instanceType, arguments).AsObject;
+end;
+
+class function TActivator.CreateInstance<T>: T;
+begin
+  Result := CreateInstance(TypeInfo(T)).AsType<T>;
+end;
+
+class function TActivator.CreateInstance<T>(
+  const arguments: array of TValue): T;
+var
+  instanceType: TRttiInstanceType;
+begin
+  instanceType := TType.GetType(TypeInfo(T)) as TRttiInstanceType;
+  Result := CreateInstance(instanceType, arguments).AsType<T>;
 end;
 
 {$ENDREGION}
