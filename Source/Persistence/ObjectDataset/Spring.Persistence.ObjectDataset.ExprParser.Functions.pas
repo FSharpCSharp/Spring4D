c37aa399f414061b2974c97f64605cbeef7eef3e
Stefan Glienke <sglienke@dsharp.org>
2015-01-06 18:16:19 +0100

    added designtime package for TObjectDataset (only XE7 for now)

--- Source/Persistence/ObjectDataset/Spring.Persistence.ObjectDataset.ExprParser.Functions.pas
+++ Source/Persistence/ObjectDataset/Spring.Persistence.ObjectDataset.ExprParser.Functions.pas
@@ -1,82 +1,93 @@
+{***************************************************************************}
+{                                                                           }
+{           Spring Framework for Delphi                                     }
+{                                                                           }
+{           Copyright (c) 2009-2014 Spring4D Team                           }
+{                                                                           }
+{           http://www.spring4d.org                                         }
+{                                                                           }
+{***************************************************************************}
+{                                                                           }
+{  Licensed under the Apache License, Version 2.0 (the "License");          }
+{  you may not use this file except in compliance with the License.         }
+{  You may obtain a copy of the License at                                  }
+{                                                                           }
+{      http://www.apache.org/licenses/LICENSE-2.0                           }
+{                                                                           }
+{  Unless required by applicable law or agreed to in writing, software      }
+{  distributed under the License is distributed on an "AS IS" BASIS,        }
+{  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. }
+{  See the License for the specific language governing permissions and      }
+{  limitations under the License.                                           }
+{                                                                           }
+{***************************************************************************}
+
+{$I Spring.inc}
+
 unit Spring.Persistence.ObjectDataset.ExprParser.Functions;
 
 interface
 
 uses
-  Generics.Collections
-  ,SysUtils
-  ;
+  Spring.Collections,
+  SysUtils;
 
 type
-  TFunctionGetValueProc = reference to function(const Args: Variant): Variant;
+  TFunctionGetValueProc = reference to function(const args: Variant): Variant;
 
-  TFilterFunctions = class sealed
+  TFilterFunctions = record
   private
     class var
-      FFunctions: TDictionary<string,TFunctionGetValueProc>;
-      FIsoFormatSettings: TFormatSettings;
-  protected
+      fFunctions: IDictionary<string,TFunctionGetValueProc>;
+      fIsoFormatSettings: TFormatSettings;
+  private
+    class procedure Initialize; static;
     class constructor Create;
-    class destructor Destroy;
   public
-    class procedure RegisterFunction(const AName: string; AGetValueFunc: TFunctionGetValueProc);
-    class procedure UnregisterFunction(const AName: string);
-    class function TryGetFunction(const AName: string; out AGetValueFunc: TFunctionGetValueProc): Boolean;
+    class procedure RegisterFunction(const name: string;
+      const func: TFunctionGetValueProc); static;
+    class procedure UnregisterFunction(const name: string); static;
+    class function TryGetFunction(const name: string;
+      out func: TFunctionGetValueProc): Boolean; static;
   end;
 
 implementation
 
 uses
-  Generics.Defaults
-  ,DateUtils
-  ,Variants
-  ,Math
-  ,StrUtils
-  ;
+  DateUtils,
+  Math,
+  StrUtils,
+  Variants;
 
-{ TFilterFunctions }
+
+{$REGION 'TFilterFunctions'}
 
 class constructor TFilterFunctions.Create;
-var
-  LCaseInsensitiveComparer: IEqualityComparer<string>;
 begin
-  LCaseInsensitiveComparer := TEqualityComparer<string>.Construct(
-    function(const Left, Right: string): Boolean
-    begin
-      Result := SameText(Left, Right);
-    end,
-    function(const Value: string): Integer
-    var s: string;
-    begin
-      s := UpperCase(Value);
-      Result := BobJenkinsHash(s[1], Length(s) * SizeOf(s[1]), 0);
-    end
-  );
-  FIsoFormatSettings := TFormatSettings.Create;
-  FIsoFormatSettings.ShortDateFormat := 'YYYY-MM-DD';
-  FIsoFormatSettings.DateSeparator := '-';
-  FIsoFormatSettings.DecimalSeparator := '.';
-  FFunctions := TDictionary<string,TFunctionGetValueProc>.Create(50, LCaseInsensitiveComparer);
+  fIsoFormatSettings := TFormatSettings.Create;
+  fIsoFormatSettings.ShortDateFormat := 'YYYY-MM-DD';
+  fIsoFormatSettings.DateSeparator := '-';
+  fIsoFormatSettings.DecimalSeparator := '.';
+
+  fFunctions := TCollections.CreateDictionary<string, TFunctionGetValueProc>(
+    50, TStringComparer.OrdinalIgnoreCase);
 end;
 
-class destructor TFilterFunctions.Destroy;
+class procedure TFilterFunctions.RegisterFunction(const name: string;
+  const func: TFunctionGetValueProc);
 begin
-  FFunctions.Free;
+  fFunctions.AddOrSetValue(name, func);
 end;
 
-class procedure TFilterFunctions.RegisterFunction(const AName: string; AGetValueFunc: TFunctionGetValueProc);
+class function TFilterFunctions.TryGetFunction(const name: string;
+  out func: TFunctionGetValueProc): Boolean;
 begin
-  FFunctions.AddOrSetValue(AName, AGetValueFunc);
+  Result := fFunctions.TryGetValue(name, func);
 end;
 
-class function TFilterFunctions.TryGetFunction(const AName: string; out AGetValueFunc: TFunctionGetValueProc): Boolean;
+class procedure TFilterFunctions.UnregisterFunction(const name: string);
 begin
-  Result := FFunctions.TryGetValue(AName, AGetValueFunc);
-end;
-
-class procedure TFilterFunctions.UnregisterFunction(const AName: string);
-begin
-  FFunctions.Remove(AName);
+  fFunctions.Remove(name);
 end;
 
 function VarArrayLength(AValue: Variant): Integer;
@@ -85,162 +96,142 @@ begin
   if Result >= 0 then
     Inc(Result)
   else
-  begin
     Result := 0;
-  end;
 end;
 
-procedure RegisterMainFunctions;
+class procedure TFilterFunctions.Initialize;
 begin
-  TFilterFunctions.RegisterFunction('today',
-    function(const Args: Variant): Variant
+  RegisterFunction('Today',
+    function(const args: Variant): Variant
     begin
-      Assert((VarArrayLength(Args) = 0), 'Today does not require any argument');
+      Assert(VarArrayLength(Args) = 0, 'Today does not require any argument');
       Result := Today;
-    end
-    );
-  TFilterFunctions.RegisterFunction('getdate',
-    function(const Args: Variant): Variant
+    end);
+  RegisterFunction('GetDate',
+    function(const args: Variant): Variant
     begin
       Result := Now;
-    end
-    );
-  TFilterFunctions.RegisterFunction('IsNull',
-    function(const Args: Variant): Variant
+    end);
+  RegisterFunction('IsNull',
+    function(const args: Variant): Variant
     begin
-      Assert((VarArrayLength(Args) = 2), 'IsNull requires 2 arguments');
+      Assert(VarArrayLength(Args) = 2, 'IsNull requires 2 arguments');
       if VarIsNull(Args[0]) then
         Result := Args[1]
       else
         Result := Args[0];
-    end
-    );
-  TFilterFunctions.RegisterFunction('Abs',
-    function(const Args: Variant): Variant
+    end);
+  RegisterFunction('Abs',
+    function(const args: Variant): Variant
     begin
       Assert(VarArrayLength(Args) = 1, 'Abs requires 1 argument');
       Result := Abs(Args[0]);
-    end
-    );
-  TFilterFunctions.RegisterFunction('IsNotNull',
-    function(const Args: Variant): Variant
+    end);
+  RegisterFunction('IsNotNull',
+    function(const args: Variant): Variant
     begin
       Result := 1;
-    end
-    );
-  TFilterFunctions.RegisterFunction('lower',
-    function(const Args: Variant): Variant
+    end);
+  RegisterFunction('Lower',
+    function(const args: Variant): Variant
     begin
-      Assert(VarArrayLength(Args) = 1, 'lower requires 1 argument');
+      Assert(VarArrayLength(Args) = 1, 'Lower requires 1 argument');
       Result := AnsiLowerCase(Args[0]);
-    end
-    );
-  TFilterFunctions.RegisterFunction('upper',
-    function(const Args: Variant): Variant
+    end);
+  RegisterFunction('Upper',
+    function(const args: Variant): Variant
     begin
-      Assert(VarArrayLength(Args) = 1, 'upper requires 1 argument');
+      Assert(VarArrayLength(Args) = 1, 'Upper requires 1 argument');
       Result := AnsiUpperCase(Args[0]);
-    end
-    );
-  TFilterFunctions.RegisterFunction('coalesce',
-    function(const Args: Variant): Variant
+    end);
+  RegisterFunction('Coalesce',
+    function(const args: Variant): Variant
     var
       i: Integer;
     begin
-      Assert(VarArrayLength(Args) >= 2, 'coalesce requires at least 2 arguments');
+      Assert(VarArrayLength(Args) >= 2, 'Coalesce requires at least 2 arguments');
       for i := VarArrayLowBound(Args, 1) to VarArrayHighBound(Args, 1) do
-      begin
         if not VarIsNull(Args[i]) then
-        begin
           Exit(Args[i]);
-        end;
-      end;
       Result := Null;
-    end
-    );
-  TFilterFunctions.RegisterFunction('Random',
-    function(const Args: Variant): Variant
+    end);
+  RegisterFunction('Random',
+    function(const args: Variant): Variant
     begin
       Assert(VarArrayLength(Args) = 1, 'Random requires 1 argument');
       Result := Random(Args[0]);
-    end
-    );
-  TFilterFunctions.RegisterFunction('Round',
-    function(const Args: Variant): Variant
+    end);
+  RegisterFunction('Round',
+    function(const args: Variant): Variant
     begin
       Assert(VarArrayLength(Args) = 1, 'Round requires 1 argument');
       Result := Round(Args[0]);
-    end
-    );
-  TFilterFunctions.RegisterFunction('Trim',
-    function(const Args: Variant): Variant
+    end);
+  RegisterFunction('Trim',
+    function(const args: Variant): Variant
     begin
       Assert(VarArrayLength(Args) = 1, 'Trim requires 1 argument');
       Result := Trim(Args[0]);
-    end
-    );
-  TFilterFunctions.RegisterFunction('substr',
-    function(const Args: Variant): Variant
+    end);
+  RegisterFunction('SubStr',
+    function(const args: Variant): Variant
+    const
+      NumericVarTypes = [varSmallint, varInteger, varShortInt, varByte,
+        varWord, varLongWord, varInt64, varUInt64];
     begin
-      Assert(VarArrayLength(Args) = 3, 'substr requires 3 arguments');
-      Assert( (VarType(Args[1]) in [varInteger, varSmallint, varInt64, varLongWord, varUInt64, varWord, varByte, varShortInt]), 'Index argument must be Integer' ) ;
-      Assert( (VarType(Args[2]) in [varInteger, varSmallint, varInt64, varLongWord, varUInt64, varWord, varByte, varShortInt]), 'Length argument must be Integer' ) ;
+      Assert(VarArrayLength(Args) = 3, 'SubStr requires 3 arguments');
+      Assert(VarType(Args[1]) in NumericVarTypes, 'Index argument must be Integer') ;
+      Assert(VarType(Args[2]) in NumericVarTypes, 'Length argument must be Integer') ;
       Result := Copy(Args[0], Integer(Args[1]), Integer(Args[2]));
-    end
-    );
-  TFilterFunctions.RegisterFunction('Quote',
-    function(const Args: Variant): Variant
+    end);
+  RegisterFunction('Quote',
+    function(const args: Variant): Variant
     begin
       Assert(VarArrayLength(Args) = 1, 'Quote requires 1 argument');
       Result := QuotedStr(Args[0]);
-    end
-    );
-  TFilterFunctions.RegisterFunction('Max',
-    function(const Args: Variant): Variant
+    end);
+  RegisterFunction('Max',
+    function(const args: Variant): Variant
     begin
       Assert(VarArrayLength(Args) = 2, 'Max requires 2 arguments');
       Result := Max(Args[0], Args[1]);
-    end
-    );
-  TFilterFunctions.RegisterFunction('Min',
-    function(const Args: Variant): Variant
+    end);
+  RegisterFunction('Min',
+    function(const args: Variant): Variant
     begin
       Assert(VarArrayLength(Args) = 2, 'Min requires 2 arguments');
       Result := Min(Args[0], Args[1]);
-    end
-    );
-  TFilterFunctions.RegisterFunction('Length',
-    function(const Args: Variant): Variant
+    end);
+  RegisterFunction('Length',
+    function(const args: Variant): Variant
     begin
       Assert(VarArrayLength(Args) = 1, 'Length requires 1 argument');
       Result := Length(Args[0]);
-    end
-    );
-  TFilterFunctions.RegisterFunction('Instr',
-    function(const Args: Variant): Variant
+    end);
+  RegisterFunction('InStr',
+    function(const args: Variant): Variant
     begin
-      Assert(VarArrayLength(Args) = 2, 'Instr requires 2 arguments');
+      Assert(VarArrayLength(Args) = 2, 'InStr requires 2 arguments');
       Result := PosEx(Args[0], Args[1]);
-    end
-    );
-  TFilterFunctions.RegisterFunction('Replace',
-    function(const Args: Variant): Variant
+    end);
+  RegisterFunction('Replace',
+    function(const args: Variant): Variant
     begin
       Assert(VarArrayLength(Args) = 3, 'Replace requires 3 arguments');
       Result := ReplaceText(Args[0], Args[1], Args[2]);
-    end
-    );
-  TFilterFunctions.RegisterFunction('Date',
-    function(const Args: Variant): Variant
+    end);
+  RegisterFunction('Date',
+    function(const args: Variant): Variant
     begin
       Assert(VarArrayLength(Args) = 1, 'Date requires 1 argument');
-      Result := StrToDate(Args[0], TFilterFunctions.FIsoFormatSettings);
-    end
-    );
-
+      Result := StrToDate(Args[0], fIsoFormatSettings);
+    end);
 end;
 
+{$ENDREGION}
+
+
 initialization
-  RegisterMainFunctions;
+  TFilterFunctions.Initialize;
 
 end.
