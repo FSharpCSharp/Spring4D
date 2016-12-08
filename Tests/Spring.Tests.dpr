7ec4faded9f58190930f3bb6f71452e7db6cff8b
Stefan Glienke <sglienke@dsharp.org>
2016-12-08 13:58:24 +0100

    changed ifdef to ifndef to make the IDE show all units

--- Tests/Spring.Tests.dpr
+++ Tests/Spring.Tests.dpr
@@ -51,11 +51,11 @@ uses
   Spring.Tests.Container.LifetimeManager in 'Source\Core\Spring.Tests.Container.LifetimeManager.pas',
   Spring.Tests.Container in 'Source\Core\Spring.Tests.Container.pas',
   Spring.Tests.Container.Logging in 'Source\Core\Spring.Tests.Container.Logging.pas',
-  {$IFDEF DELPHIXE_UP}
+{$IFNDEF DELPHI2010}
   Spring.Tests.Interception in 'Source\Core\Spring.Tests.Interception.pas',
   Spring.Tests.Interception.Types in 'Source\Core\Spring.Tests.Interception.Types.pas',
   Spring.Tests.Mocking in 'Source\Core\Spring.Tests.Mocking.pas',
-  {$ENDIF }
+{$ENDIF}
   Spring.Tests.Pool in 'Source\Core\Spring.Tests.Pool.pas',
   Spring.Tests.Cryptography in 'Source\Extensions\Spring.Tests.Cryptography.pas',
   Spring.Tests.Testing in 'Source\Spring.Tests.Testing.pas',
