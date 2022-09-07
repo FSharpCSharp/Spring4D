4c74efca0e4292c8f12e92b05f180af7c5ab6037
Stefan Glienke <sglienke@dsharp.org>
2022-09-07 18:19:29 +0200

    fixed defect with address calculation

--- Source/Extensions/Cryptography/Spring.Cryptography.SHA.pas
+++ Source/Extensions/Cryptography/Spring.Cryptography.SHA.pas
@@ -481,7 +481,7 @@ var
 begin
   Move(md.state, S, 64);
   for i := 0 to 15 do
-    W[i] := ByteSwap64(PUInt64(UInt32(@md.buf) + i * 8)^);
+    W[i] := ByteSwap64(PUInt64(@md.buf)[i]);
   for i := 16 to 79 do
     W[i] := Gamma1(W[i - 2]) + W[i - 7] + Gamma0(W [i - 15]) + W[i - 16];
   for i := 0 to 79 do
